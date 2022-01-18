90f1a03d68016343fa372439ec012928785ace0c
Carolina Martinez <dalia_6486@hotmail.com>
2022-01-18 12:46:25 -0600

    Add alias to imports

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import OrderItem from '../components/OrderItem';
-import MyOrder  from '../containers/MyOrder';
-import '../styles/Orders.scss';
+import OrderItem from '@components/OrderItem';
+import MyOrder  from '@containers/MyOrder';
+import '@styles/Orders.scss';
 
 const Orders = () => {
     return (
