624ecd5bab742ba04261976a6256631c5e1a3442
Carolina Martinez <dalia_6486@hotmail.com>
2022-01-18 12:13:51 -0600

    [ADD] FILE AND GITHUB ACTIONS

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@styles/NotFound.scss'
+
+const NotFound = () => {
+    return (
+        <div className='content-not-found'>
+            OPSSS...NOT FOUND
+            <img src="./no-found.gif" className='img-gif'/>
+        </div>
+    )
+}
+
+export default NotFound
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { dirname } = require('path');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias : {
+			'@components' : path.resolve(__dirname, 'src/components/'),
+			'@containers' : path.resolve(__dirname, 'src/containers/'),
+			'@pages' : path.resolve(__dirname, 'src/pages/'),
+			'@styles' : path.resolve(__dirname, 'src/styles/'),
+			'@icons' : path.resolve(__dirname, 'src/assets/icons'),
+			'@logos' : path.resolve(__dirname, 'src/assets/logos/'),
+			'@context' : path.resolve(__dirname, 'src/context/')
+		}
 	},
 	module: {
 		rules: [
@@ -30,12 +41,16 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.(css|scss)$/,
 				use: [
 					"style-loader",
 					"css-loader",
 					"sass-loader",
 				],
+			},
+			{
+				test : /\.(png|svg|jpg|gif)$/,
+				type : 'asset'
 			}
 		]
 	},
@@ -48,4 +63,7 @@ module.exports = {
 			filename: '[name].css'
 		}),
 	],
+	devServer:{
+		historyApiFallback: true
+	}
 }
