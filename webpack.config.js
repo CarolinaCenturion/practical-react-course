15e3ea6b8d82ea168f87bc7ec8b163abc4a81768
Carolina Martinez <dalia_6486@hotmail.com>
2022-01-18 14:33:53 -0600

    set bundle

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: 'practical-react-course/bundle.js',
 		publicPath: '/'
 	},
 	mode: 'development',
